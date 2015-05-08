4eab84b43c300d08fdd10cbcdaef100281668ae6
rogersb11 <brettrogers11@gmail.com>
2015-05-08 02:06:29 +0200

    Forward Port: PIE 3.0 (1/2)
    Ported to lollipop and modified by Brett Rogers(CyanideL) & BlackDragon(Fusion).
    
    PIE Key contributors:
    @BigBrother1984
    @D4rKn3sSyS
    @Anu6is
    @David96
    @drcmda
    
    P19:
       - Renamed pie directories
       - Renamed some pie class names
       - Modify copyright information
       - Use PieConstants into PieController class
    
    P20:
      fix BaseStatusBar class in reflection of P19 changes
    
    P21:
        - fix piehelper class in reference to P19
    
    P22:
       - fix piecontroller reference to P19
    
    P24:
      - fix crashes in pie control xml
    
    Note: Made to work with Marshmallow by xanaxdroid
    
    Signed-off-by: rogersb11 <brettrogesr11@gmail.com>
    Change-Id: I1838a300544bd5c769d82f1193065731baebdb48
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/custom_config.xml
    	packages/SystemUI/res/values/screwd_dimens.xml
    	packages/SystemUI/res/values/screwd_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    	services/core/java/com/android/server/statusbar/StatusBarManagerService.java
    
    Screw'd:
    Make PIE co-exist with DUI & squash fixes
    
    Fix Oversized kill task icon:
    https://github.com/ScrewdAOSP/frameworks_base/commit/78169b3cf044e1acc5dfe9efd3951b73ddcb51d0
    
    Pie: Disable background in Bare mode:
    https://github.com/ScrewdAOSP/frameworks_base/commit/141042305aedb976b7d503e6e1b128a16938cc74
    
    Pie: Do not translate Java date formats:
    https://github.com/ScrewdAOSP/frameworks_base/commit/26deb610f9aa2eece7e8dc39247d8bf9c427d424
    
    Pie: Always on the right switch by @LorDClockaN
    https://github.com/ScrewdAOSP/frameworks_base/commit/a0133eb7ed42e8354493df7a71aab1ded71ccb7f
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1559,12 +1559,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                     | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                     | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH
+                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
                 PixelFormat.TRANSLUCENT);
         // this will allow the navbar to run in an overlay on devices that support this
-        if (ActivityManager.isHighEndGfx()) {
+/*        if (ActivityManager.isHighEndGfx()) {
             lp.flags |= WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
-        }
+        }*/ //*Keep for possible future use.
 
         lp.setTitle("NavigationBar");
         lp.windowAnimations = 0;
@@ -3021,6 +3022,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationController.getBar() != null) {
             mNavigationController.getBar().setNavigationIconHints(hints);
         }
+        if (mPieController != null) {
+            mPieController.setNavigationIconHints(hints);
+        }
         checkBarModes();
     }
 
@@ -3295,6 +3299,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     public void topAppWindowChanged(boolean showMenu) {
+        if (mPieController != null && mPieController.getControlPanel() != null)
+            mPieController.getControlPanel().setMenu(showMenu);
+
         if (SPEW) {
             Log.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
         }
@@ -3645,6 +3652,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 notifyHeadsUpScreenOff();
                 finishBarAnimations();
                 resetUserExpandedStates();
+                // detach PA Pie when screen is turned off
+                if (mPieController != null) mPieController.detachPie();
             }
             else if (Intent.ACTION_SCREEN_ON.equals(action)) {
                 notifyNavigationBarScreenOn(true);
