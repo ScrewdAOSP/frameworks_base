026c1a42d25a986abf62036f4bfe203634b2b663
d34d <clark@cyngn.com>
2016-01-15 12:07:17 -0800

    Themes: Fix applying theme for notifications
    Notifications should be inflated using the current theme applied
    to SystemUI.  This was not happening because the Context created
    when applying a RemoteView was returning a cahced context rather
    than one with the supplied theme applied.
    
    This patch also moves mCurrentTheme into BaseStatusBar so it can
    be used when inflating the remote views for notifications.
    
    Change-Id: I577ae30fc02028c7e497cd774f79029f8a045d1f
    TICKET: CYNGNOS-1608
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -313,6 +313,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     private ArrayList<String> mBlacklist = new ArrayList<String>();
     private ArrayList<String> mWhitelist = new ArrayList<String>();
 
+    // last theme that was applied in order to detect theme change (as opposed
+    // to some other configuration change).
+    protected ThemeConfig mCurrentTheme;
+
     @Override  // NotificationData.Environment
     public boolean isDeviceProvisioned() {
         return mDeviceProvisioned;
@@ -1630,9 +1634,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         View contentViewLocal = null;
         View bigContentViewLocal = null;
         View headsUpContentViewLocal = null;
-        final ThemeConfig themeConfig = mContext.getResources().getConfiguration().themeConfig;
-        String themePackageName = themeConfig != null ?
-                themeConfig.getOverlayPkgNameForApp(mContext.getPackageName()) : null;
+        String themePackageName = mCurrentTheme != null ?
+                mCurrentTheme.getOverlayForStatusBar() : null;
         try {
             contentViewLocal = contentView.apply(
                     sbn.getPackageContext(mContext),
