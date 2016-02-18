0180585d28b2d5731a11342bcd9b25b90f987444
Kyler <mbqsniper@gmail.com>
2016-02-18 10:56:34 -0700

    frameworks/base: Various adjustments and tweaks
    core/java/android/view/ViewConfiguration.java:
    * Adjusted the default long press timeout, felt too
    short imo
    * Adjusted the global actions key timeout, a very subtle
    adjustment
    * Adjusted a few other things too, frankly I think
    the values we had were a little too quick, but to each their
    own.
    
    packages/SettingsProvider/res/values/defaults.xml:
    * Increased the default screen brightness by a small amount
    * Small adjustment for Settings.Secure.LONG_PRESS_TIMEOUT_MILLIS
    
    packages/SystemUI/res/values/dimens.xml:
    * Increased the min and max Z index values by a little bit
    * Increased the fake shadow size and inset for Recents
    
    packages/SystemUI/res/values/screwd_dimens.xml:
    * Increased the Elevation for the volume panel a little bit
    * Increased the task manager item height by 8dip
    
    packages/SystemUI/res/values/styles.xml:
    * Changed the statusbar color to 10% black, something
    different and not too noticeable.
    * Increased the ambient shadow alpha for Recents
    
    packages/SystemUI/src/com/android/systemui/SwipeHelper.java:
    * Increased the max escape animation duration a small amount
    
    ../src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    * Adjusted a couple values
    
    services/core/java/com/android/server/am/ActivityManagerService.java:
    * Increased the app switch delay time by a very small amount.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -283,8 +283,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
             .build();
 
-    public static final int FADE_KEYGUARD_START_DELAY = 100;
-    public static final int FADE_KEYGUARD_DURATION = 300;
+    public static final int FADE_KEYGUARD_START_DELAY = 200;
+    public static final int FADE_KEYGUARD_DURATION = 400;
     public static final int FADE_KEYGUARD_DURATION_PULSING = 96;
 
     private static final float BRIGHTNESS_CONTROL_PADDING = 0.15f;
@@ -292,7 +292,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private static final int BRIGHTNESS_CONTROL_LINGER_THRESHOLD = 20;
 
     /** Allow some time inbetween the long press for back and recents. */
-    private static final int LOCK_TO_APP_GESTURE_TOLERENCE = 2000;
+    private static final int LOCK_TO_APP_GESTURE_TOLERENCE = 700;
 
     /** If true, the system is in the half-boot-to-decryption-screen state.
      * Prudently disable QS and notifications.  */
@@ -2313,7 +2313,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                                 + mBackdropBack.getDrawable());
                     }
                     mBackdropFront.animate()
-                            .setDuration(250)
+                            .setDuration(350)
                             .alpha(0f).withEndAction(mHideBackdropFront);
                 }
             }
