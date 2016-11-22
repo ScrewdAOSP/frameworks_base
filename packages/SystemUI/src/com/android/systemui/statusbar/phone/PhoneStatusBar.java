0789d96c160c39cfc8a58b757064ce65475583af
Corey Edwards <ensabahnur16@gmail.com>
2016-11-22 09:12:53 -0600

    [SQUASHED] Remove Omni Battery Styles
    Revert "Update screwd_dimens.xml (#16)"
    
    This reverts commit cd13305cc17ed8eb14a6098ed272035a4d9340e1.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "base: SystemUI: update power save status correct"
    
    This reverts commit b8818d4a4d3a10248fb680732f6c8a379354e7bb.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "Droid -> Screw'd"
    
    This reverts commit 660ed7a673176dee485df71a8656756815e6e794.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "Update Screw'd Battery overlay (#15)"
    
    This reverts commit 4fbcadcbff5b393c6d69a95f3d75140667b51bda.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "Fix frame and and bolt DarkIntensity issue"
    
    This reverts commit 32e2d37d845f397187171769313987d633eb1448.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "base: battery style: fix layout changes"
    
    This reverts commit 66c26f94815b360098daa2893ebd97ece52905f1.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "base: horizontal battery style: change charging symbol positioning"
    
    This reverts commit 14600b36bd8ffff6ceea339d3a696ce914b3a368.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Revert "[1/2] base: battery styles are back"
    
    This reverts commit 2038e0663b880ce13fe16cd8d93b2477d9bf9f9d.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    remove car stuff
    
    add back Linear
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -139,7 +139,6 @@ import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.navigation.NavigationController;
 import com.android.systemui.navigation.Navigator;
 import com.android.systemui.qs.QSContainer;
-import com.android.systemui.omni.BatteryViewManager;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
@@ -420,8 +419,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     ArrayList<Runnable> mPostCollapseRunnables = new ArrayList<>();
 
     private int mBatterySaverWarningColor;
-    // omni additions
-    private BatteryViewManager mBatteryViewManager;
 
     // for disabling the status bar
     int mDisabled1 = 0;
@@ -1078,6 +1075,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // set the inital view visibility
         setAreThereNotifications();
 
+        createIconController();
 
         // Background thread for any controllers that need it.
         mHandlerThread = new HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND);
@@ -1100,13 +1098,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 // noop
             }
         });
-
-        // must be before StatusBarIconController
-        LinearLayout batteryContainer = (LinearLayout) mStatusBarView.findViewById(R.id.battery_container);
-        mBatteryViewManager = new BatteryViewManager(mContext, batteryContainer, mStatusBarView.getBarTransitions(), null);
-
-        createIconController();
-
         mNetworkController = new NetworkControllerImpl(mContext, mHandlerThread.getLooper());
         mNetworkController.setUserSetupComplete(mUserSetup);
         mHotspotController = new HotspotControllerImpl(mContext);
@@ -1179,8 +1170,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mKeyguardStatusBar.setUserSwitcherController(mUserSwitcherController);
         mUserInfoController.reloadUserInfo();
 
-        mBatteryViewManager.setBatteryController(mBatteryController);
-        mHeader.setBatteryController(mBatteryController);
+        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
+                mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
 
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
@@ -3947,7 +3938,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         clearCurrentMediaNotification();
         mLockscreenWallpaper.setCurrentUser(newUserId);
         updateMediaMetaData(true, false);
-        mBatteryViewManager.update();
     }
 
     private void setControllerUsers() {
@@ -5427,9 +5417,5 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
         }
-    }
-
-    protected BatteryViewManager getBatteryViewManager() {
-        return mBatteryViewManager;
     }
 }
